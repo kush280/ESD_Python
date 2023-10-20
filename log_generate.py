e95171997fb882c58ff10f40dd757d5df51fed83
sachinsinghha <ss3698213@gmail.com>
2023-10-20 20:41:21 +0530

    chhhh

--- log_generate.py
+++ log_generate.py
@@ -1,21 +1,17 @@
 import datetime
 import time
-# import gputil
+import gputil
 import psutil
-
 
 def get_cpu_usage():
     return psutil.cpu_percent()
 
-
-# def get_gpu_usage():
-#     gpu = gputil.get_gpu(0)
-#     return gpu.load * 100
-
+def get_gpu_usage():
+    gpu = gputil.getGPUs()[0]
+    return gpu.load * 100
 
 def get_network_usage():
     return psutil.net_io_counters().bytes_sent + psutil.net_io_counters().bytes_recv
-
 
 def get_cpu_temperature():
     try:
@@ -23,31 +19,20 @@ def get_cpu_temperature():
     except:
         return 0
 
-
-def get_gpu_temperature():
-    if psutil.GPUs:
-        gpu = psutil.GPUs[0]
-        return gpu.temperature
-    else:
-        return 0
-
-
 def get_ram_utilization():
     return psutil.virtual_memory().percent
 
-
 def get_storage_utilization():
     return psutil.disk_usage('/').percent
-
 
 def log_system_usage():
     timestamp = datetime.datetime.now()
     log_file = open('LOGFILE.log', 'a')
     log_file.write(
-        '{timestamp},{cpu_usage},{network_usage},{cpu_temperature},{ram_utilization},{storage_utilization}\n'.format(
+        '{timestamp},{cpu_usage},{gpu_usage},{network_usage},{cpu_temperature},{ram_utilization},{storage_utilization}\n'.format(
             timestamp=timestamp,
             cpu_usage=get_cpu_usage(),
-            # gpu_usage=get_gpu_usage(),
+            gpu_usage=get_gpu_usage(),
             network_usage=get_network_usage(),
             cpu_temperature=get_cpu_temperature(),
             ram_utilization=get_ram_utilization(),
@@ -55,12 +40,10 @@ def log_system_usage():
         ))
     log_file.close()
 
-
 def main():
     while True:
         log_system_usage()
         time.sleep(1)
-
 
 if __name__ == '__main__':
     main()

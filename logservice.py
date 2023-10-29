97d409b4cdf764287b86885304a47c4b8006a2dd
sachinsingha <128895390+sachinsingha@users.noreply.github.com>
2023-10-30 02:02:08 +0530

    Update logservice.py

--- logservice.py
+++ logservice.py
@@ -1,28 +1,38 @@
-import os, sys, datetime, socket, subprocess
+import os
+import sys
+import datetime
+import socket
+import subprocess
 
 hostname = socket.gethostname()
 time = datetime.datetime.now().strftime("%y%m%d%H%M%S")
 
 with open("Ping_output_" + time + ".log", 'w') as outfile:
-    sys.stdout = outfile
-    
+    original_stdout = sys.stdout  # Save the original sys.stdout
+    sys.stdout = outfile  # Redirect sys.stdout to the file
+
     with open("IP.txt", 'r') as file:
         print(hostname, ":", datetime.datetime.now(), ":", "Reading the text file")
         t2 = " ".join([x.strip() for x in file])
         t3 = t2.split()
         print(hostname, ":", datetime.datetime.now(), ":", "Conversion of string to list")
-        
+
         for i in t3:
             try:
-                res = subprocess.Popen(["ping", "-n", "1", i], stdout=subprocess.PIPE, shell=True).communicate()[0]
-            except socket.error as ff:
+                res = subprocess.Popen(["ping", "-n", "1", i], stdout=subprocess.PIPE, shell=True)
+                res.communicate()
+            except subprocess.CalledProcessError as ff:
                 print(hostname, ":", datetime.datetime.now(), ":", ff)
-            
-            if b'unreachable' in res:
+            except Exception as e:
+                print(hostname, ":", datetime.datetime.now(), ":", "Error:", e)
+
+            if "unreachable" in res.stdout.decode():
                 print(hostname, ":", datetime.datetime.now(), ":", "Client", i, "is Down")
-            elif b'expired' in res:
+            elif "expired" in res.stdout.decode():
                 print(hostname, ":", datetime.datetime.now(), ":", "Client", i, "is Down")
-            elif b'timed out' in res:
+            elif "timed out" in res.stdout.decode():
                 print(hostname, ":", datetime.datetime.now(), ":", "Client", i, "is Down")
             else:
                 print(hostname, ":", datetime.datetime.now(), ":", "Client", i, "is UP")
+
+    sys.stdout = original_stdout  # Reset sys.stdout to the original value

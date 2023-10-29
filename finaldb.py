7861bd6d0a17d3e3c1ad798eb72cfa40fc4fd823
sachinsingha <128895390+sachinsingha@users.noreply.github.com>
2023-10-30 01:51:20 +0530

    Update finaldb.py

--- finaldb.py
+++ finaldb.py
@@ -1,33 +1,32 @@
 import time
 import psutil
 import GPUtil
-import influxdb_client
-
+from influxdb_client import InfluxDBClient
+from influxdb_client.client.write_api import SYNCHRONOUS
 
 def log_system_usage():
-    client = influxdb_client.InfluxDBClient(url="http://localhost:8086",
-                                            token="pKIqmJs3a-lTctA04-1oyH3nNuFm_KxQLbSLnXDjGd11U5GEjEKX4oFKzSN2OGtPNRqRm5nK-QBy4Jb27UndeQ==",
-                                            org="ESD_B2",
-                                            bucket="Browser")
-    point = influxdb_client.Point('Utilization')
-    cpu_usage = psutil.cpu_percent()
-    mem_usage = psutil.virtual_memory().percent
-    upload_usage = psutil.net_io_counters().bytes_sent
-    download_usage = psutil.net_io_counters().bytes_recv
-    disk_usage = psutil.disk_usage('/').percent
-    gpu_usage = GPUtil.getGPUs()[0].memoryUtil
-    gpu_temperature = GPUtil.getGPUs()[0].temperature
-    point.tag("hostname", "localhost")
-    point.field("CPU_usage", cpu_usage)
-    point.field("RAM_usage", mem_usage)
-    point.field("upload_speed", upload_usage)
-    point.field("download_speed", download_usage)
-    point.field("disk_usage", disk_usage)
-    point.field("GPU_usage", gpu_usage)
-    point.field("GPU_temperature", gpu_temperature)
+    client = InfluxDBClient(url="http://localhost:8086",
+                            token="pKIqmJs3a-lTctA04-1oyH3nNuFm_KxQLbSLnXDjGd11U5GEjEKX4oFKzSN2OGtPNRqRm5nK-QBy4Jb27UndeQ==",
+                            org="ESD_B2",
+                            bucket="Browser")
+    
+    point = {}
+    point["measurement"] = "Utilization"
+    point["tags"] = {
+        "hostname": "localhost"
+    }
+    point["fields"] = {
+        "CPU_usage": psutil.cpu_percent(),
+        "RAM_usage": psutil.virtual_memory().percent,
+        "upload_speed": psutil.net_io_counters().bytes_sent,
+        "download_speed": psutil.net_io_counters().bytes_recv,
+        "disk_usage": psutil.disk_usage('/').percent,
+        "GPU_usage": GPUtil.getGPUs()[0].memoryUtil,
+        "GPU_temperature": GPUtil.getGPUs()[0].temperature
+    }
 
-    client.write_api().write(bucket="Browser", record=point)
-
+    write_api = client.write_api(write_options=SYNCHRONOUS)
+    write_api.write(bucket="Browser", record=point)
 
 if __name__ == '__main__':
     while True:

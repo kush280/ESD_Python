427a8b34932cc8458ab265960eafc1ebfd8355a0
sachinsingha <128895390+sachinsingha@users.noreply.github.com>
2023-10-30 17:58:34 +0530

    Update 3finaldb.py

--- finaldb.py
+++ finaldb.py
@@ -9,33 +9,34 @@ def log_system_usage():
                             token="pKIqmJs3a-lTctA04-1oyH3nNuFm_KxQLbSLnXDjGd11U5GEjEKX4oFKzSN2OGtPNRqRm5nK-QBy4Jb27UndeQ==",
                             org="ESD_B2",
                             bucket="Browser")
-    
+
     point = {}
     point["measurement"] = "Utilization"
     point["tags"] = {
         "hostname": "localhost"
     }
     point["fields"] = {
-        "CPU_usage": psutil.cpu_percent(interval=1),  # Add interval to measure over 1 second
+        "CPU_usage_total": psutil.cpu_percent(),
+        "CPU_usage_interval": psutil.cpu_percent(interval=1),  
         "RAM_usage": psutil.virtual_memory().percent,
         "upload_speed": psutil.net_io_counters().bytes_sent,
         "download_speed": psutil.net_io_counters().bytes_recv,
-        "disk_usage": psutil.disk_usage('/').percent,
+        "disk_usage": psutil.disk_usage('/').percent
     }
-    
+
     gpus = GPUtil.getGPUs()
     if gpus:
         point["fields"]["GPU_usage"] = gpus[0].memoryUtil
         point["fields"]["GPU_temperature"] = gpus[0].temperature
     else:
-        point["fields"]["GPU_usage"] = None  # Handle the case where no GPUs are found
+        point["fields"]["GPU_usage"] = None  
         point["fields"]["GPU_temperature"] = None
 
     write_api = client.write_api(write_options=SYNCHRONOUS)
     write_api.write(bucket="Browser", record=point)
 
-if __name__ == '__main__':
-    while True:
-        log_system_usage()
-        time.sleep(1)
+while True:
+    log_system_usage()
+    time.sleep(1)
+
 

30f43723db1f3e2e7b23676b357d975dcdcf2004
sachinsingha <128895390+sachinsingha@users.noreply.github.com>
2023-10-30 14:20:45 +0530

    Update2 finaldb.py

--- finaldb.py
+++ finaldb.py
@@ -16,14 +16,20 @@ def log_system_usage():
         "hostname": "localhost"
     }
     point["fields"] = {
-        "CPU_usage": psutil.cpu_percent(),
+        "CPU_usage": psutil.cpu_percent(interval=1),  # Add interval to measure over 1 second
         "RAM_usage": psutil.virtual_memory().percent,
         "upload_speed": psutil.net_io_counters().bytes_sent,
         "download_speed": psutil.net_io_counters().bytes_recv,
         "disk_usage": psutil.disk_usage('/').percent,
-        "GPU_usage": GPUtil.getGPUs()[0].memoryUtil,
-        "GPU_temperature": GPUtil.getGPUs()[0].temperature
     }
+    
+    gpus = GPUtil.getGPUs()
+    if gpus:
+        point["fields"]["GPU_usage"] = gpus[0].memoryUtil
+        point["fields"]["GPU_temperature"] = gpus[0].temperature
+    else:
+        point["fields"]["GPU_usage"] = None  # Handle the case where no GPUs are found
+        point["fields"]["GPU_temperature"] = None
 
     write_api = client.write_api(write_options=SYNCHRONOUS)
     write_api.write(bucket="Browser", record=point)
@@ -32,3 +38,4 @@ if __name__ == '__main__':
     while True:
         log_system_usage()
         time.sleep(1)
+
